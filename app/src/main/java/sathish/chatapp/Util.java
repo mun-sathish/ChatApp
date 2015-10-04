fead4d825bad19581f88299b28ed3fcf78268e22
Sathish Mun <mun.sathish@gmail.com>
2015-10-04 14:12:28 +0530

    new: usr: Chat App completed

---
+++ app/src/main/java/sathish/chatapp/Util.java
@@ -0,0 +1,21 @@
+package sathish.chatapp;
+
+
+public class Util {
+
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    public static final String PROPERTY_APP_VERSION = "appVersion";
+    public static final String EMAIL = "email";
+    public static final String USER_NAME = "user_name";
+
+    public final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public final static String SENDER_ID = "386371411276";
+
+    public static String base_url = "http://www.sathishmun.comule.com/GcmPHP/";
+
+    public final static String register_url = base_url + "register.php";
+    public final static String send_chat_url = base_url + "sendChatmessage.php";
+
+
+}
